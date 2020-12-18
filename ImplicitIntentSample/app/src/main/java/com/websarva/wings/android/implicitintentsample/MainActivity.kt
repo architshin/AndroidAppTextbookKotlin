33b1b9ff47267ba30bdb271779277dae90ad6089
Shinzo SAITO <architshin@websarva.com>
2020-12-18 21:08:14 +0900

    14.2節まで完了。

--- ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
+++ ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.websarva.wings.android.implicitintentsample
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import java.net.URLEncoder
 
 /**
@@ -27,48 +19,17 @@ import java.net.URLEncoder
  */
 class MainActivity : AppCompatActivity() {
 	/**
-	 * 緯度フィールド。
+	 * 緯度プロパティ。
 	 */
 	private var _latitude = 0.0
 	/**
-	 * 経度フィールド
+	 * 経度プロパティ。
 	 */
 	private var _longitude = 0.0
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
-		//LocationManagerオブジェクトを取得。
-		val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-		//位置情報が更新された際のリスナオブジェクトを生成。
-		val locationListener = GPSLocationListener()
-		//ACCESS_FINE_LOCATIONの許可が下りていないなら…
-		if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			//ACCESS_FINE_LOCATIONの許可を求めるダイアログを表示。その際、リクエストコードを1000に設定。
-			val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-			ActivityCompat.requestPermissions(this@MainActivity, permissions, 1000)
-			//onCreate()メソッドを終了。
-			return
-		}
-		//位置情報の追跡を開始。
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-	}
-
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-		//ACCESS_FINE_LOCATIONに対するパーミションダイアログでかつ許可を選択したなら…
-		if(requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-			//LocationManagerオブジェクトを取得。
-			val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-			//位置情報が更新された際のリスナオブジェクトを生成。
-			val locationListener = GPSLocationListener()
-			//再度ACCESS_FINE_LOCATIONの許可が下りていないかどうかのチェックをし、降りていないなら処理を中止。
-			if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-				return
-			}
-			//位置情報の追跡を開始。
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-		}
 	}
 
 	/**
@@ -102,29 +63,5 @@ class MainActivity : AppCompatActivity() {
 		val intent = Intent(Intent.ACTION_VIEW, uri)
 		//アクティビティを起動。
 		startActivity(intent)
-	}
-
-	/**
-	 * ロケーションリスナクラス。
-	 */
-	private inner class GPSLocationListener : LocationListener {
-		override fun onLocationChanged(location: Location) {
-			//引数のLocationオブジェクトから緯度を取得。
-			_latitude = location.latitude
-			//引数のLocationオブジェクトから経度を取得。
-			_longitude = location.longitude
-			//取得した緯度をTextViewに表示。
-			val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
-			tvLatitude.text = _latitude.toString()
-			//取得した経度をTextViewに表示。
-			val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
-			tvLongitude.text = _longitude.toString()
-		}
-
-		override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-
-		override fun onProviderEnabled(provider: String) {}
-
-		override fun onProviderDisabled(provider: String) {}
 	}
 }
