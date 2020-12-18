14385c643399bd34d5b25898c77a47ef00a697fa
Shinzo SAITO <architshin@websarva.com>
2020-12-19 03:10:24 +0900

    14.3節まで終了。

--- ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
+++ ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.websarva.wings.android.implicitintentsample
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.*
 import java.net.URLEncoder
 
 /**
@@ -26,10 +28,52 @@ class MainActivity : AppCompatActivity() {
 	 * 経度プロパティ。
 	 */
 	private var _longitude = 0.0
+	/**
+	 * FusedLocationProviderClientオブジェクトプロパティ。
+	 */
+	private lateinit var _fusedLocationClient: FusedLocationProviderClient
+	/**
+	 * LocationRequestオブジェクトプロパティ。
+	 */
+	private lateinit var _locationRequest: LocationRequest
+	/**
+	 * 位置情報が変更された時の処理を行うコールバックオブジェクトプロパティ。
+	 */
+	private lateinit var _onUpdateLocation: OnUpdateLocation
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		// FusedLocationProviderClientオブジェクトを取得。
+		_fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+		// LocationRequestオブジェクトを生成。
+		_locationRequest = LocationRequest.create()
+		// LocationRequestオブジェクトがnullでないなら…
+		_locationRequest?.let {
+			// 位置情報の最短更新間隔を設定。
+			it.interval = 5000
+			// 位置情報の最短更新間隔を設定。
+			it.fastestInterval = 1000
+			// 位置情報の取得精度を設定。
+			it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+		}
+		// 位置情報が変更された時の処理を行うコールバックオブジェクトを生成。
+		_onUpdateLocation = OnUpdateLocation()
+	}
+
+	override fun onResume() {
+		super.onResume()
+
+		// 位置情報の追跡を開始。
+		_fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation, mainLooper)
+	}
+
+	override fun onPause() {
+		super.onPause()
+
+		// 位置情報の追跡を停止。
+		_fusedLocationClient.removeLocationUpdates(_onUpdateLocation)
 	}
 
 	/**
@@ -63,5 +107,29 @@ class MainActivity : AppCompatActivity() {
 		val intent = Intent(Intent.ACTION_VIEW, uri)
 		//アクティビティを起動。
 		startActivity(intent)
+	}
+
+	/**
+	 * 位置情報が変更された時の処理を行うコールバッククラス。
+	 */
+	private inner class OnUpdateLocation : LocationCallback() {
+		override fun onLocationResult(locationResult: LocationResult?) {
+			locationResult?.let {
+				// 直近の位置情報を取得。
+				val location = it.lastLocation
+				location?.let {
+					// locationオブジェクトから緯度を取得。
+					_latitude = it.latitude
+					// locationオブジェクトから経度を取得。
+					_longitude = it.longitude
+					// 取得した緯度をTextViewに表示。
+					val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
+					tvLatitude.text = _latitude.toString()
+					// 取得した経度をTextViewに表示。
+					val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
+					tvLongitude.text = _longitude.toString()
+				}
+			}
+		}
 	}
 }
