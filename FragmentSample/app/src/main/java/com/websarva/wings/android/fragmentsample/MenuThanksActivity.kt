812f951422e62a1e83df363bd24f24c589a5b667
Shinzo SAITO <architshin@websarva.com>
2020-12-26 03:53:12 +0900

    第9章「フラグメントサンプル」を第2版に合わせて改変。 (#13)

--- FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksActivity.kt
+++ FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksActivity.kt
@@ -1,10 +1,10 @@
 package com.websarva.wings.android.fragmentsample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 /**
- * 『Androidアプリ開発の教科書』
+ * 『Androidアプリ開発の教科書Kotlin』
  * 第9章
  * フラグメントサンプル
  *
