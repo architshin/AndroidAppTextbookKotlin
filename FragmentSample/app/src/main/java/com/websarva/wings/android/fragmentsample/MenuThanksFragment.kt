e94585bd6bf6afb9a8abc603a34b1729d21cdfe6
Shinzo SAITO <architshin@websarva.com>
2020-12-26 03:52:14 +0900

    第9章「フラグメントサンプル」を第2版に合わせて改変。

--- FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksFragment.kt
+++ FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksFragment.kt
@@ -1,7 +1,7 @@
 package com.websarva.wings.android.fragmentsample
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 
 /**
- * 『Androidアプリ開発の教科書』
+ * 『Androidアプリ開発の教科書Kotlin』
  * 第9章
  * フラグメントサンプル
  *
@@ -26,11 +26,11 @@ class MenuThanksFragment : Fragment() {
 	private var _isLayoutXLarge = true
 
 	override fun onCreate(savedInstanceState: Bundle?) {
-		//親クラスのonCreate()の呼び出し。
+		// 親クラスのonCreate()の呼び出し。
 		super.onCreate(savedInstanceState)
-		//フラグメントマネージャーからメニューリストフラグメントを取得。
+		// フラグメントマネージャーからメニューリストフラグメントを取得。
 		val menuListFragment = fragmentManager?.findFragmentById(R.id.fragmentMenuList)
-		//メニューリストフラグメントがnull、つまり存在しないなら…
+		// メニューリストフラグメントがnull、つまり存在しないなら…
 		if(menuListFragment == null) {
 			//画面判定フラグを通常画面とする。
 			_isLayoutXLarge = false
@@ -38,37 +38,37 @@ class MenuThanksFragment : Fragment() {
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		//フラグメントで表示する画面をXMLファイルからインフレートする。
+		// フラグメントで表示する画面をXMLファイルからインフレートする。
 		val view = inflater.inflate(R.layout.fragment_menu_thanks, container, false)
 
-		//Bundleオブジェクトを宣言。
+		// Bundleオブジェクトを宣言。
 		val extras: Bundle?
-		//大画面の場合…
+		// 大画面の場合…
 		if(_isLayoutXLarge) {
-			//このフラグメントに埋め込まれた引き継ぎデータを取得。
+			// このフラグメントに埋め込まれた引き継ぎデータを取得。
 			extras = arguments
 		}
-		//通常画面の場合…
+		// 通常画面の場合…
 		else {
-			//所属アクティビティからインテントを取得。
+			// 所属アクティビティからインテントを取得。
 			val intent = activity?.intent
-			//インテントから引き継ぎデータをまとめたもの(Bundleオブジェクト)を取得。
+			// インテントから引き継ぎデータをまとめたもの(Bundleオブジェクト)を取得。
 			extras = intent?.extras
 		}
 
-		//定食名と金額を取得。
+		// 定食名と金額を取得。
 		val menuName = extras?.getString("menuName")
 		val menuPrice = extras?.getString("menuPrice")
-		//定食名と金額を表示させるTextViewを取得。
+		// 定食名と金額を表示させるTextViewを取得。
 		val tvMenuName = view.findViewById<TextView>(R.id.tvMenuName)
 		val tvMenuPrice = view.findViewById<TextView>(R.id.tvMenuPrice)
-		//TextViewに定食名と金額を表示。
+		// TextViewに定食名と金額を表示。
 		tvMenuName.text = menuName
 		tvMenuPrice.text = menuPrice
 
-		//戻るボタンを取得。
+		// 戻るボタンを取得。
 		val btBackButton = view.findViewById<Button>(R.id.btBackButton)
-		//戻るボタンにリスナを登録。
+		// 戻るボタンにリスナを登録。
 		btBackButton.setOnClickListener(ButtonClickListener())
 
 		//インフレートされた画面を戻り値として返す。
@@ -80,18 +80,18 @@ class MenuThanksFragment : Fragment() {
 	 */
 	private inner class ButtonClickListener : View.OnClickListener {
 		override fun onClick(view: View) {
-			//大画面の場合…
+			// 大画面の場合…
 			if(_isLayoutXLarge) {
-				//フラグメントトランザクションの開始。
+				// フラグメントトランザクションの開始。
 				val transaction = fragmentManager?.beginTransaction()
-				//自分自身を削除。
+				// 自分自身を削除。
 				transaction?.remove(this@MenuThanksFragment)
-				//フラグメントトランザクションのコミット。
+				// フラグメントトランザクションのコミット。
 				transaction?.commit()
 			}
-			//通常画面の場合…
+			// 通常画面の場合…
 			else {
-				//自分が所属するアクティビティを終了。
+				// 自分が所属するアクティビティを終了。
 				activity?.finish()
 			}
 		}
