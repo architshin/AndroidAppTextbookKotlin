548899b286c61fcad8b8d2adbae25887acd17643
Shinzo SAITO <architshin@websarva.com>
2020-12-25 17:03:10 +0900

    第8章「メニューサンプル」を第2版に合わせて改変。 (#11)
    * P.177まで終了。
    
    * 8-1節まで終了。
    
    * 8-2節まで終了。
    
    * 最後まで完成。
    
    * コメントで変なところを修正。

--- MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.websarva.wings.android.menusample
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.Menu
@@ -11,9 +10,10 @@ import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 /**
- * 『Androidアプリ開発の教科書』
+ * 『Androidアプリ開発の教科書Kotlin』
  * 第8章
  * メニューサンプル
  *
@@ -25,97 +25,107 @@ class MainActivity : AppCompatActivity() {
 	/**
 	 * リストビューに表示するリストデータ。
 	 */
-	private var _menuList: MutableList<MutableMap<String, Any>>? = null
+	private var _menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
 	/**
-	 * SimpleAdapterの第4引数fromに使用する定数フィールド。
+	 * SimpleAdapterの第4引数fromに使用するプロパティ。
 	 */
-	private val FROM = arrayOf("name", "price")
+	private val _from = arrayOf("name", "price")
 	/**
-	 * SimpleAdapterの第5引数toに使用する定数フィールド。
+	 * SimpleAdapterの第5引数toに使用するプロパティ。
 	 */
-	private val TO = intArrayOf(R.id.tvMenuName, R.id.tvMenuPrice)
+	private val _to = intArrayOf(R.id.tvMenuName, R.id.tvMenuPrice)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		//SimpleAdapterで使用する定食メニューListオブジェクトをprivateメソッドを利用して用意し、フィールドに格納。
+		// SimpleAdapterで使用する定食メニューListオブジェクトをprivateメソッドを利用して用意し、フィールドに格納。
 		_menuList = createTeishokuList()
-		//画面部品ListViewを取得。
+		// 画面部品ListViewを取得。
 		val lvMenu = findViewById<ListView>(R.id.lvMenu)
-		//SimpleAdapterを生成。
-		val adapter = SimpleAdapter(applicationContext, _menuList, R.layout.row, FROM, TO)
-		//アダプタの登録。
+		// SimpleAdapterを生成。
+		val adapter = SimpleAdapter(this@MainActivity, _menuList, R.layout.row, _from, _to)
+		// アダプタの登録。
 		lvMenu.adapter = adapter
-		//リストタップのリスナクラス登録。
+		// リストタップのリスナクラス登録。
 		lvMenu.onItemClickListener = ListItemClickListener()
 
-		//コンテキストメニューをリストビューに登録。
+		// コンテキストメニューをリストビューに登録。
 		registerForContextMenu(lvMenu)
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		//オプションメニュー用xmlファイルをインフレイト。
+		// オプションメニュー用xmlファイルをインフレイト。
 		menuInflater.inflate(R.menu.menu_options_menu_list, menu)
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
-		return super.onCreateOptionsMenu(menu)
+		// trueをリターン。
+		return true
 	}
 
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		//選択されたメニューのIDのR値による処理の分岐。
+		// 戻り値用の変数を初期値trueで用意。
+		var returnVal = true
+		// 選択されたメニューのIDのR値による処理の分岐。
 		when(item.itemId) {
-			//定食メニューが選択された場合の処理。
+			// 定食メニューが選択された場合の処理。
 			R.id.menuListOptionTeishoku ->
-				//定食メニューリストデータの生成。
+				// 定食メニューリストデータの生成。
 				_menuList = createTeishokuList()
-			//カレーメニューが選択された場合の処理。
+			// カレーメニューが選択された場合の処理。
 			R.id.menuListOptionCurry ->
-				//カレーメニューリストデータの生成。
+				// カレーメニューリストデータの生成。
 				_menuList = createCurryList()
+			// それ以外…
+			else ->
+				// 親クラスの同名メソッドを呼び出し、その戻り値をreturnValとする。
+				returnVal = super.onOptionsItemSelected(item)
 		}
-		//画面部品ListViewを取得。
+		// 画面部品ListViewを取得。
 		val lvMenu = findViewById<ListView>(R.id.lvMenu)
-		//SimpleAdapterを生成。
-		val adapter = SimpleAdapter(applicationContext, _menuList, R.layout.row, FROM, TO)
-		//アダプタの登録。
+		// SimpleAdapterを選択されたメニューデータで生成。
+		val adapter = SimpleAdapter(this@MainActivity, _menuList, R.layout.row, _from, _to)
+		// アダプタの登録。
 		lvMenu.adapter = adapter
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
-		return super.onOptionsItemSelected(item)
+		return returnVal
 	}
 
 	override fun onCreateContextMenu(menu: ContextMenu, view: View, menuInfo: ContextMenu.ContextMenuInfo) {
-		//親クラスの同名メソッドの呼び出し。
+		// 親クラスの同名メソッドの呼び出し。
 		super.onCreateContextMenu(menu, view, menuInfo)
-		//コンテキストメニュー用xmlファイルをインフレイト。
+		// コンテキストメニュー用xmlファイルをインフレイト。
 		menuInflater.inflate(R.menu.menu_context_menu_list, menu)
-		//コンテキストメニューのヘッダタイトルを設定。
+		// コンテキストメニューのヘッダタイトルを設定。
 		menu.setHeaderTitle(R.string.menu_list_context_header)
 	}
 
 	override fun onContextItemSelected(item: MenuItem): Boolean {
-		//長押しされたビューに関する情報が格納されたオブジェクトを取得。
+		// 戻り値用の変数を初期値trueで用意。
+		var returnVal = true
+		// 長押しされたビューに関する情報が格納されたオブジェクトを取得。
 		val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
-		//長押しされたリストのポジションを取得。
+		// 長押しされたリストのポジションを取得。
 		val listPosition = info.position
-		//ポジションから長押しされたメニュー情報Mapオブジェクトを取得。
-		val menu = _menuList!![listPosition]
+		// ポジションから長押しされたメニュー情報Mapオブジェクトを取得。
+		val menu = _menuList[listPosition]
 
-		//選択されたメニューのIDのR値による処理の分岐。
+		// 選択されたメニューのIDのR値による処理の分岐。
 		when(item.itemId) {
-			//［説明を表示］メニューが選択された時の処理。
+			// ［説明を表示］メニューが選択された時の処理。
 			R.id.menuListContextDesc -> {
-				//メニューの説明文字列を取得。
+				// メニューの説明文字列を取得。
 				val desc = menu["desc"] as String
-				//トーストを表示。
+				// トーストを表示。
 				Toast.makeText(applicationContext, desc, Toast.LENGTH_LONG).show()
 			}
-			//［ご注文］メニューが選択された時の処理。
+			// ［ご注文］メニューが選択された時の処理。
 			R.id.menuListContextOrder ->
-				//注文処理。
+				// 注文処理。
 				order(menu)
+			// それ以外…
+			else ->
+				// 親クラスの同名メソッドを呼び出し、その戻り値をreturnValとする。
+				returnVal = super.onContextItemSelected(item)
 		}
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
-		return super.onContextItemSelected(item)
+		return returnVal
 	}
 
 	/**
@@ -124,15 +134,15 @@ class MainActivity : AppCompatActivity() {
 	 * @return 生成された定食リストデータ。
 	 */
 	private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
-		//定食メニューリスト用のListオブジェクトを用意。
+		// 定食メニューリスト用のListオブジェクトを用意。
 		val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
-		//「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		var menu = mutableMapOf<String, Any>("name" to "から揚げ定食", "price" to 800, "desc" to "若鳥のから揚げにサラダ、ご飯とお味噌汁が付きます。")
 		menuList.add(menu)
-		//「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		menu = mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
 		menuList.add(menu)
-		//以下データ登録の繰り返し。
+		// 以下データ登録の繰り返し。
 		menu = mutableMapOf("name" to "生姜焼き定食", "price" to 850, "desc" to "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。")
 		menuList.add(menu)
 		menu = mutableMapOf("name" to "ステーキ定食", "price" to 1000, "desc" to "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。")
@@ -147,6 +157,12 @@ class MainActivity : AppCompatActivity() {
 		menuList.add(menu)
 		menu = mutableMapOf("name" to "コロッケ定食", "price" to 850, "desc" to "北海道ポテトコロッケにサラダ、ご飯とお味噌汁が付きます。")
 		menuList.add(menu)
+		menu = mutableMapOf("name" to "回鍋肉定食", "price" to 750, "desc" to "ピリ辛回鍋肉にサラダ、ご飯とお味噌汁が付きます。")
+		menuList.add(menu)
+		menu = mutableMapOf("name" to "麻婆豆腐定食", "price" to 800, "desc" to "本格四川風麻婆豆腐にサラダ、ご飯とお味噌汁が付きます。")
+		menuList.add(menu)
+		menu = mutableMapOf("name" to "青椒肉絲定食", "price" to 900, "desc" to "ピーマンの香り豊かな青椒肉絲にサラダ、ご飯とお味噌汁が付きます。")
+		menuList.add(menu)
 		menu = mutableMapOf("name" to "焼き魚定食", "price" to 850, "desc" to "鰆の塩焼きにサラダ、ご飯とお味噌汁が付きます。")
 		menuList.add(menu)
 		menu = mutableMapOf("name" to "焼肉定食", "price" to 950, "desc" to "特性たれの焼肉にサラダ、ご飯とお味噌汁が付きます。")
@@ -160,15 +176,15 @@ class MainActivity : AppCompatActivity() {
 	 * @return 生成されたカレーリストデータ。
 	 */
 	private fun createCurryList(): MutableList<MutableMap<String, Any>> {
-		//カレーメニューリスト用のListオブジェクトを用意。
+		// カレーメニューリスト用のListオブジェクトを用意。
 		val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
-		//「ビーフカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「ビーフカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		var menu = mutableMapOf<String, Any>("name" to "ビーフカレー", "price" to 520, "desc" to "特選スパイスをきかせた国産ビーフ100%のカレーです。")
 		menuList.add(menu)
-		//「ポークカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「ポークカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		menu = mutableMapOf("name" to "ポークカレー", "price" to 420, "desc" to "特選スパイスをきかせた国産ポーク100%のカレーです。")
 		menuList.add(menu)
-		//以下データ登録の繰り返し。
+		// 以下データ登録の繰り返し。
 		menu = mutableMapOf("name" to "ハンバーグカレー", "price" to 620, "desc" to "特選スパイスをきかせたカレーに手ごねハンバーグをトッピングです。")
 		menuList.add(menu)
 		menu = mutableMapOf("name" to "チーズカレー", "price" to 560, "desc" to "特選スパイスをきかせたカレーにとろけるチーズをトッピングです。")
@@ -188,18 +204,18 @@ class MainActivity : AppCompatActivity() {
 	 * @param menu 注文するメニューを表すMapオブジェクト。
 	 */
 	private fun order(menu: MutableMap<String, Any>) {
-		//定食名と金額を取得。Mapの値部分がAny型なのでキャストが必要。
+		// 定食名と金額を取得。Mapの値部分がAny型なのでキャストが必要。
 		val menuName = menu["name"] as String
 		val menuPrice = menu["price"] as Int
 
-		//インテントオブジェクトを生成。
-		val intent = Intent(applicationContext, MenuThanksActivity::class.java)
-		//第2画面に送るデータを格納。
-		intent.putExtra("menuName", menuName)
-		//MenuThanksActivityでのデータ受け取りと合わせるために、金額にここで「円」を追加する。
-		intent.putExtra("menuPrice", "${menuPrice}円")
-		//第2画面の起動。
-		startActivity(intent)
+		// インテントオブジェクトを生成。
+		val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+		// 第2画面に送るデータを格納。
+		intent2MenuThanks.putExtra("menuName", menuName)
+		// MenuThanksActivityでのデータ受け取りと合わせるために、金額にここで「円」を追加する。
+		intent2MenuThanks.putExtra("menuPrice", "${menuPrice}円")
+		// 第2画面の起動。
+		startActivity(intent2MenuThanks)
 	}
 
 	/**
@@ -207,9 +223,9 @@ class MainActivity : AppCompatActivity() {
 	 */
 	private inner class ListItemClickListener : AdapterView.OnItemClickListener {
 		override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-			//タップされた行のデータを取得。
+			// タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMutableMap型!
 			val item = parent.getItemAtPosition(position) as MutableMap<String, Any>
-			//注文処理。
+			// 注文処理。
 			order(item)
 		}
 	}
