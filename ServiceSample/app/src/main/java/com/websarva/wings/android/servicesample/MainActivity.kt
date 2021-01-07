4935b183219a1094906a9a0da70b466054e4c8b2
Shinzo SAITO <architshin@websarva.com>
2021-01-08 01:45:56 +0900

    第13章「サービスサンプル」を第2版に合わせて改変。

--- ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/MainActivity.kt
+++ ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.websarva.wings.android.servicesample
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
 /**
- * 『Androidアプリ開発の教科書』
+ * 『Androidアプリ開発の教科書Kotlin』
  * 第13章
  * サービスサンプル
  *
@@ -20,11 +20,11 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		//Intentから通知のタップからの引き継ぎデータを取得。
+		// Intentから通知のタップからの引き継ぎデータを取得。
 		val fromNotification = intent.getBooleanExtra("fromNotification", false)
-		//引き継ぎデータが存在、つまり通知のタップからならば…
+		// 引き継ぎデータが存在、つまり通知のタップからならば…
 		if(fromNotification) {
-			//再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
+			// 再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
 			val btPlay = findViewById<Button>(R.id.btPlay)
 			val btStop = findViewById<Button>(R.id.btStop)
 			btPlay.isEnabled = false
@@ -38,11 +38,11 @@ class MainActivity : AppCompatActivity() {
 	 * @param view 画面部品
 	 */
 	fun onPlayButtonClick(view: View) {
-		//インテントオブジェクトを生成。
-		val intent = Intent(applicationContext, SoundManageService::class.java)
-		//サービスを起動。
+		// インテントオブジェクトを生成。
+		val intent = Intent(this@MainActivity, SoundManageService::class.java)
+		// サービスを起動。
 		startService(intent)
-		//再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
+		// 再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
 		val btPlay = findViewById<Button>(R.id.btPlay)
 		val btStop = findViewById<Button>(R.id.btStop)
 		btPlay.isEnabled = false
@@ -55,11 +55,11 @@ class MainActivity : AppCompatActivity() {
 	 * @param view 画面部品
 	 */
 	fun onStopButtonClick(view: View) {
-		//インテントオブジェクトを生成。
-		val intent = Intent(applicationContext, SoundManageService::class.java)
-		//サービスを停止。
+		// インテントオブジェクトを生成。
+		val intent = Intent(this@MainActivity, SoundManageService::class.java)
+		// サービスを停止。
 		stopService(intent)
-		//再生ボタンをタップ可に、停止ボタンをタップ不可に変更。
+		// 再生ボタンをタップ可に、停止ボタンをタップ不可に変更。
 		val btPlay = findViewById<Button>(R.id.btPlay)
 		val btStop = findViewById<Button>(R.id.btStop)
 		btPlay.isEnabled = true
