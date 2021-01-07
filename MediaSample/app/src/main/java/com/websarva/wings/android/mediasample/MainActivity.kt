6390c53d8d12fc506820941cfd4029fbeddf110c
Shinzo SAITO <architshin@websarva.com>
2021-01-07 17:21:30 +0900

    第12章「メディアサンプル」を第2版に合わせて改変。

--- MediaSample/app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
+++ MediaSample/app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.kt
@@ -2,18 +2,15 @@ package com.websarva.wings.android.mediasample
 
 import android.media.MediaPlayer
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CompoundButton
-import android.widget.Switch
-import java.io.IOException
-import java.lang.IllegalArgumentException
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 /**
- * 『Androidアプリ開発の教科書』
+ * 『Androidアプリ開発の教科書Kotlin』
  * 第12章
  * メディアサンプル
  *
@@ -23,7 +20,7 @@ import java.lang.IllegalArgumentException
  */
 class MainActivity : AppCompatActivity() {
 	/**
-	 * メディアプレーヤーフィールド。
+	 * メディアプレーヤープロパティ。
 	 */
 	private var _player: MediaPlayer? = null
 
@@ -31,50 +28,45 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		//フィールドのメディアプレーヤーオブジェクトを生成。
+		// プロパティのメディアプレーヤーオブジェクトを生成。
 		_player = MediaPlayer()
-		//音声ファイルのURI文字列を作成。
+		// 音声ファイルのURI文字列を作成。
 		val mediaFileUriStr = "android.resource://${packageName}/${R.raw.mountain_stream}"
-		//音声ファイルのURI文字列を元にURIオブジェクトを生成。
+		// 音声ファイルのURI文字列を元にURIオブジェクトを生成。
 		val mediaFileUri = Uri.parse(mediaFileUriStr)
-		try {
-			//メディアプレーヤーに音声ファイルを指定。
-			_player?.setDataSource(applicationContext, mediaFileUri)
-			//非同期でのメディア再生準備が完了した際のリスナを設定。
-			_player?.setOnPreparedListener(PlayerPreparedListener())
-			//メディア再生が終了した際のリスナを設定。
-			_player?.setOnCompletionListener(PlayerCompletionListener())
-			//非同期でメディア再生を準備。
-			_player?.prepareAsync()
-		}
-		catch(ex: IllegalArgumentException) {
-			Log.e("MediaSample", "メディアプレーヤー準備時の例外発生", ex)
-		}
-		catch(ex: IOException) {
-			Log.e("MediaSample", "メディアプレーヤー準備時の例外発生", ex)
+		// プロパティのプレーヤーがnullじゃなかったら
+		_player?.let {
+			// メディアプレーヤーに音声ファイルを指定。
+			it.setDataSource(this@MainActivity, mediaFileUri)
+			// 非同期でのメディア再生準備が完了した際のリスナを設定。
+			it.setOnPreparedListener(PlayerPreparedListener())
+			// メディア再生が終了した際のリスナを設定。
+			it.setOnCompletionListener(PlayerCompletionListener())
+			// 非同期でメディア再生を準備。
+			it.prepareAsync()
 		}
 
-		//スイッチを取得。
-		val loopSwitch = findViewById<Switch>(R.id.swLoop)
-		//スイッチにリスナを設定。
+		// スイッチを取得。
+		val loopSwitch = findViewById<SwitchMaterial>(R.id.swLoop)
+		// スイッチにリスナを設定。
 		loopSwitch.setOnCheckedChangeListener(LoopSwitchChangedListener())
 	}
 
 	override fun onDestroy() {
-		//親クラスのメソッド呼び出し。
-		super.onDestroy()
-		//フィールドのプレーヤーがnullじゃなかったら
+		// プロパティのプレーヤーがnullじゃなかったら
 		_player?.let {
-			//プレーヤーが再生中なら…
+			// プレーヤーが再生中なら…
 			if(it.isPlaying) {
-				//プレーヤーを停止。
+				// プレーヤーを停止。
 				it.stop()
 			}
-			//プレーヤーを解放。
+			// プレーヤーを解放。
 			it.release()
-			//プレーヤー用フィールドをnullに。
-			_player = null
 		}
+		// プレーヤー用プロパティをnullに。
+		_player = null
+		// 親クラスのメソッド呼び出し。
+		super.onDestroy()
 	}
 
 	/**
@@ -83,22 +75,22 @@ class MainActivity : AppCompatActivity() {
 	 * @param view 画面部品
 	 */
 	fun onPlayButtonClick(view: View) {
-		//フィールドのプレーヤーがnullじゃなかったら
+		// プロパティのプレーヤーがnullじゃなかったら
 		_player?.let {
-			//再生ボタンを取得。
+			// 再生ボタンを取得。
 			val btPlay = findViewById<Button>(R.id.btPlay)
-			//プレーヤーが再生中だったら…
+			// プレーヤーが再生中だったら…
 			if(it.isPlaying) {
-				//プレーヤーを一時停止。
+				// プレーヤーを一時停止。
 				it.pause()
-				//再生ボタンのラベルを「再生」に設定。
+				// 再生ボタンのラベルを「再生」に設定。
 				btPlay.setText(R.string.bt_play_play)
 			}
-			//プレーヤーが再生中じゃなかったら…
+			// プレーヤーが再生中じゃなかったら…
 			else {
-				//プレーヤーを再生。
+				// プレーヤーを再生。
 				it.start()
-				//再生ボタンのラベルを「一時停止」に設定。
+				// 再生ボタンのラベルを「一時停止」に設定。
 				btPlay.setText(R.string.bt_play_pause)
 			}
 		}
@@ -110,7 +102,7 @@ class MainActivity : AppCompatActivity() {
 	 * @param view 画面部品
 	 */
 	fun onBackButtonClick(view: View) {
-		//再生位置を先頭に変更。
+		// 再生位置を先頭に変更。
 		_player?.seekTo(0)
 	}
 
@@ -120,15 +112,15 @@ class MainActivity : AppCompatActivity() {
 	 * @param view 画面部品
 	 */
 	fun onForwardButtonClick(view: View) {
-		//フィールドのプレーヤーがnullじゃなかったら
+		// プロパティのプレーヤーがnullじゃなかったら
 		_player?.let {
-			//現在再生中のメディファイルの長さを取得。
+			// 現在再生中のメディファイルの長さを取得。
 			val duration = it.duration
-			//再生位置を終端に変更。
+			// 再生位置を終端に変更。
 			it.seekTo(duration)
-			//再生中でないなら…
+			// 再生中でないなら…
 			if(!it.isPlaying) {
-				//再生を開始。
+				// 再生を開始。
 				it.start()
 			}
 		}
@@ -139,7 +131,7 @@ class MainActivity : AppCompatActivity() {
 	 */
 	private inner class PlayerPreparedListener : MediaPlayer.OnPreparedListener {
 		override fun onPrepared(mp: MediaPlayer) {
-			//各ボタンをタップ可能に設定。
+			// 各ボタンをタップ可能に設定。
 			val btPlay = findViewById<Button>(R.id.btPlay)
 			btPlay.isEnabled = true
 			val btBack = findViewById<Button>(R.id.btBack)
@@ -154,11 +146,11 @@ class MainActivity : AppCompatActivity() {
 	 */
 	private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener {
 		override fun onCompletion(mp: MediaPlayer) {
-			//フィールドのプレーヤーがnullじゃなかったら
+			// プロパティのプレーヤーがnullじゃなかったら
 			_player?.let {
-				//ループ設定がされていないならば…
+				// ループ設定がされていないならば…
 				if(!it.isLooping) {
-					//再生ボタンのラベルを「再生」に設定。
+					// 再生ボタンのラベルを「再生」に設定。
 					val btPlay = findViewById<Button>(R.id.btPlay)
 					btPlay.setText(R.string.bt_play_play)
 				}
@@ -171,7 +163,7 @@ class MainActivity : AppCompatActivity() {
 	 */
 	private inner class LoopSwitchChangedListener : CompoundButton.OnCheckedChangeListener {
 		override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
-			//ループするかどうかを設定。
+			// ループするかどうかを設定。
 			_player?.isLooping = isChecked
 		}
 	}
