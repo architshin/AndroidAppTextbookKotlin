faf6b0148c13d5e984bc3baf799d546c051f3dfc
Shinzo SAITO <architshin@websarva.com>
2022-07-17 01:28:48 +0900

    第5章「リスト選択サンプル2」を第3版に合わせて改変。

--- ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 		// ListViewオブジェクトを取得。
 		val lvMenu = findViewById<ListView>(R.id.lvMenu)
 		// リストビューに表示するリストデータを作成。
-		var menuList = mutableListOf("から揚げ定食", "ハンバーグ定食", "生姜焼き定食", "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食", "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "焼き魚定食", "焼肉定食")
+		var menuList = mutableListOf("から揚げ定食", "ハンバーグ定食", "生姜焼き定食", "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食", "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "八宝菜定食", "酢豚定食", "焼き魚定食", "焼肉定食")
 		// アダプタオブジェクトを生成。
 		val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
 		// リストビューにアダプタオブジェクトを設定。
